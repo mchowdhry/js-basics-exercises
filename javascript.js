afe2c1ae501bf1054769d4d9004232c8ee6d5900
Muhammad Chowdhry <muhammad.chowdhry@gmail.com>
2023-05-31 12:15:45 +0100

    worked halfway through js basics fund 1

---
+++ javascript.js
@@ -0,0 +1,74 @@
+/*
+
+//FUNDAMENTALS PART 1
+
+//Variables
+
+let message;
+message = 'Hello';
+alert(message);
+
+let user = John;
+let age = 25;
+let message = 'Hello';
+
+let message;
+message = 'Hello';
+message = 'World!';
+alert(message);
+
+let hello = 'Hello World!';
+let message;
+message=hello;
+alert(hello);
+alert(message);
+
+
+let $ = 1;
+let _ = 2;
+alert($ + _);
+
+
+
+//Constants
+
+const myBirthday = '18.04.1987';
+alert(myBirthday);
+
+
+
+const COLOR_RED = "#F00";
+const COLOR_GREEN = "#0F0";
+const COLOR_BLUE = "00F";
+const COLOR_ORANGE = "#FF7F00";
+
+let color = COLOR_ORANGE;
+alert(color);
+
+
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
+
+let ourPlanetName = "Earth";
+let currentUserName = "John";
+
+
+
+//Js Arithmetic
+
+let x = 100 + 50;
+alert(x);
+
+
+let a = 100;
+let b = 50;
+let x = a + b;
+alert(x);
+
+*/
+
+let a = 3;
+let x = (100 + 50) * a;
+alert (x);
